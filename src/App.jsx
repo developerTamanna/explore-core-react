444457d132474d85bb037b041c7c4b76e5b58a43
developerTamanna <astamanna57@gmail.com>
2025-03-23 11:21:48 +0600

    normal component create

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,17 @@ function App() {
     <> 
       <h1>Vite + React</h1>
        <Person></Person>
+       <Person></Person>
+       <Person></Person>
     </>
 
   )
 }
 function Person(){
+  const age = 15;
+  const name = "sathi"
   return(
-    <p>i m a person</p>
+    <p>i m a person . my name is  {name} , and my age:{age}</p>
   )
 }
 
