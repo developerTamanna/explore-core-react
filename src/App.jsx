30d25845ea32b82882c0bea1d44929f970ae1f04
developerTamanna <astamanna57@gmail.com>
2025-03-23 15:18:23 +0600

    style add

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,73 @@ function App() {
 
   return (
     <> 
-      <h1>Vite + React</h1>
-      <Pet></Pet>
-      <Sports></Sports>
-       <Person></Person>
-       <Person></Person>
-       <Person></Person>
-      <Sports></Sports>
+      <h1>React core concept</h1>
+
+    <Student></Student>
+    <Student></Student>
+    <Person></Person>
+    <Developer name="Tamanna" tech="Js"></Developer>
+    <Developer name="Lima" tech="Python"></Developer>
+    <Developer name="Ruba" tech="React"></Developer>
     </>
 
   )
 }
+
+
+function Developer(props){
+  console.log(props)
+  return(
+    <div style={{
+       color: 'purple',
+       fontSize: '20px',
+       border: '2px solid blue',
+       borderRadius: '10px',
+       marginTop: '10px'
+    }}>
+      <h3>Developer: {props.name}</h3>
+      <p>Technology: {props.tech}</p>
+    </div>
+  )
+}
+
+
+
+
+
 function Person(){
   const age = 15;
   const name = "sathi"
+  const PersonStyle ={
+     color:'red',
+     fontSize:'20px',
+     textAlign: 'right', 
+
+  }
   return(
-    <p>i m a person . my name is  {name} , and my age:{age}</p>
+    <p style={PersonStyle}>I m a person . my name is  {name} , and my age is:{age}</p>
   )
 }
+
+function Student(){
+  return(
+    <div className='student' >
+      <p>Name:</p>
+      <p>Dep:</p>
+    </div>
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
 
 function Sports(){
   return(
