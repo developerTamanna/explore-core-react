a3049913bbb3027b74af6a7b371818fdf5b4f785
developerTamanna <astamanna57@gmail.com>
2025-03-23 15:55:56 +0600

    device

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,19 @@ function App() {
     <Student></Student>
     <Student></Student>
     <Person></Person>
-    <Developer name="Tamanna" tech="Js"></Developer>
-    <Developer name="Lima" tech="Python"></Developer>
-    <Developer name="Ruba" tech="React"></Developer>
+    <Developer ins="Sylhet polytechnic institute" name="Tamanna" tech="Js"></Developer>
+    <Developer ins="Leading University Sylhet" name="Lima" tech="Python"></Developer>
+    <Developer ins="Dhaka Engineering University" name="Ruba" tech="React"></Developer>
+   <Electric></Electric>
+    <Device name="i phone" price="20000" color="black" brand="apple">
+      
+       </Device>
+    <Device name="samsung" price="3000" color="gold" brand="smsng">
+      
+       </Device>
+    <Device name="readmy" price="90000" color="green" brand="readmy">
+      
+       </Device>
     </>
 
   )
@@ -23,12 +33,15 @@ function Developer(props){
   console.log(props)
   return(
     <div style={{
+      padding: '5px',
        color: 'purple',
        fontSize: '20px',
        border: '2px solid blue',
        borderRadius: '10px',
        marginTop: '10px'
     }}>
+
+      <h2>Institute: {props.ins} </h2>
       <h3>Developer: {props.name}</h3>
       <p>Technology: {props.tech}</p>
     </div>
@@ -36,6 +49,33 @@ function Developer(props){
 }
 
 
+function Electric(){
+  return(
+    <h2>electric device</h2>
+  )
+}
+
+function Device(device){
+  console.log(device)
+  return(
+    
+    <>
+    
+    <div style={{
+      border:'2px solid red',
+      color: 'green',
+      padding: '5px',
+      borderRadius: '7px',
+      marginTop: '7px'
+    }}>
+      <h3>Name: {device.name}</h3>
+      <h3>Price: { device.price}</h3>
+      <h3>Color: {device.color}</h3>
+      <h3>Brand: {device.brand}</h3>
+    </div>
+    </>
+  )
+}
 
 
 
